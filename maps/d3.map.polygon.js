68e7cef53438229cae1bd391a0ff7efbf1dcd97b
Saigesp <saigesp@gmail.com>
2018-10-13 23:52:38 +0200

    add fitGeojson method

--- maps/d3.map.polygon.js
+++ maps/d3.map.polygon.js
@@ -7,6 +7,7 @@ class D3MapPolygon extends D3Map {
 
     loadGeojson(geojson, removePre=true) {
         var self = this;
+        this.geojson = geojson
 
         if(removePre) this.removeGeojsons()
 
@@ -28,6 +29,12 @@ class D3MapPolygon extends D3Map {
         if(this.geojsonLayer){
             this.map.removeLayer(this.geojsonLayer);
         }
+    }
+
+    fitGeojson(geojson=this.geojson){
+        var bounds = this.getGeojsonBounds(geojson)
+        var zoombounds = L.latLngBounds(bounds[0], bounds[1]);
+        this.fitBounds(zoombounds, {animate: true})
     }
 
     loadPoints(points){
@@ -54,6 +61,42 @@ class D3MapPolygon extends D3Map {
         self.circles[platform].forEach(function(item){
             self.map.removeLayer(item)
         })
+    }
+
+    getGeojsonBounds(geojson){
+        var all = [[180,90],[-180,-90]]
+
+        function iLl(ll){
+            for(let i = 0; i<2; i++){
+                if(ll[i] < all[0][i]) all[0][i] = ll[i];
+                if(ll[i] > all[1][i]) all[1][i] = ll[i];
+            }
+        }
+        function iPl(pl){ pl.forEach((ll)=>{ iLl(ll) }) }
+        function iPls(pls){ pls.forEach((pl)=>{ iPl(pl) }) }
+        function iMpls(mpls){ mpls.forEach((pls)=>{ iPls(pls) }) }
+        function iFs(fts){
+            fts.forEach((ft)=>{
+                ft.geometry.coordinates.forEach((pls)=>{
+                    pls.forEach((pl)=>{
+                        if(ft.geometry.type=='MultiPolygon') iPl(pl)
+                        else iLl(pl)
+                    })
+                })
+            })
+        }
+
+        if (geojson.type == 'FeatureCollection'){
+            iFs(geojson.features)
+
+        } else if (geojson.type == 'MultiPolygon'){
+            iMpls(geojson.coordinates)
+
+        }else if(geojson.type == 'Polygon'){
+            iPls(geojson.coordinates)
+        }
+
+        return [[all[0][1],all[0][0]], [all[1][1],all[1][0]]]
     }
 
 }
