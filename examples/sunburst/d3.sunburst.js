efff0d56cb6186692a757be02f17d777e99f1e6d
Santi Espinosa <saigesp@gmail.com>
2019-07-30 12:11:48 +0200

    basic sunburst

---
+++ examples/sunburst/d3.sunburst.js
@@ -0,0 +1,103 @@
+class SunBurst{
+    constructor(selection, data, config = {}) {
+        let self = this;
+        this.selection = selection;
+        this.data = data;
+
+        // Graph configuration
+        this.cfg = {
+            'margin': {'top': 30, 'right': 70, 'bottom': 36, 'left': 10},
+            'key': 'name',
+            'value': 'value',
+            //'colorScale': d3.interpolateRdBu,
+            //'label': false,
+            //'label_x': 35,
+            //'label_y': 14,
+            //'units': 'ºC',
+            //'legendRanges': false, // [13,14,15,16]
+        };
+        Object.keys(config).forEach(function(key) {
+            if(config[key] instanceof Object && config[key] instanceof Array === false){
+                Object.keys(config[key]).forEach(function(sk) {
+                    self.cfg[key][sk] = config[key][sk];
+                });
+            } else self.cfg[key] = config[key];
+        });
+
+        this.cfg.width = parseInt(this.selection.node().offsetWidth) - this.cfg.margin.left - this.cfg.margin.right,
+        this.cfg.height = parseInt(this.selection.node().offsetHeight)- this.cfg.margin.top - this.cfg.margin.bottom;
+
+        let radius = Math.min(this.cfg.width, this.cfg.height)/2;
+
+        const xScale = d3.scaleLinear()
+            .range([0, 2 * Math.PI])
+            .clamp(true);
+
+        const yScale = d3.scaleSqrt()
+            .range([radius*.1, radius]);
+
+        this.arc = d3.arc()
+            .startAngle(function(d){ return xScale(d.x0)})
+            .endAngle(function(d){ return xScale(d.x1)})
+            .innerRadius(function(d){ return Math.max(0, yScale(d.y0))})
+            .outerRadius(function(d){ return Math.max(0, yScale(d.y1))});
+
+        this.initGraph();
+    }
+
+    initGraph() {
+        let self = this;
+
+
+        this.svg = this.selection.append('svg')
+            .attr("class", "chart barchart")
+            .attr("viewBox", "0 0 "+(this.cfg.width + this.cfg.margin.left + this.cfg.margin.right)+" "+(this.cfg.height + this.cfg.margin.top + this.cfg.margin.bottom))
+            .attr("width", this.cfg.width + this.cfg.margin.left + this.cfg.margin.right)
+            .attr("height", this.cfg.height + this.cfg.margin.top + this.cfg.margin.bottom);
+
+        this.g = this.svg.append("g")
+            .attr("transform", "translate(" + (self.cfg.margin.left) + "," + (self.cfg.margin.top) + ")");
+
+        this.cg = this.g.append("g")
+            .attr("transform", "translate(" + (self.cfg.width/2) + "," + (self.cfg.height/2) + ")");
+
+
+        let partition = d3.partition();
+
+        let root = d3.hierarchy(this.data);
+        root.sum(function(d){
+            return d[self.cfg.value];
+        });
+
+        let slice = this.cg.selectAll('g.slice')
+            .data(partition(root).descendants());
+
+        slice.exit().remove();
+
+
+        let newSlice = slice.enter()
+            .append('g').attr('class', 'slice')
+            .on('click', d => {
+                d3.event.stopPropagation();
+                //focusOn(d);
+            });
+
+        newSlice.append('title')
+            .text(function(d){
+                return d.data.name
+            });
+
+        newSlice.append('path')
+            .attr('class', 'main-arc')
+            .style('fill', function(d){
+                return 'steelblue';
+            })
+            .attr('d', this.arc);
+/*
+        newSlice.append('path')
+            .attr('class', 'hidden-arc')
+            .attr('id', (_, i) => `hiddenArc${i}`)
+            .attr('d', middleArcLine);
+*/
+    }
+}
